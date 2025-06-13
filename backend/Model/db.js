2c66d497c1e83691fd39b9a1c1cb830e2016a319
AurinkoHealthCare <aurinkohealthcare2014@gmail.com>
2025-06-13 13:48:37 +0530

    my backend

--- backend/Model/db.js
+++ backend/Model/db.js
@@ -2,6 +2,6 @@ const mongoose=require('mongoose')
 
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log(err));
